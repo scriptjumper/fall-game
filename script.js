4411465f9ddac4d46fdf114bf2cce43134595ea9
scriptjumper <shaeendev@gmail.com>
2021-09-02 20:08:36 +0200

    Fixing left and right movement

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 var character = document.getElementById("character");
 var interval;
+var both = 0;
 
 function moveLeft() {
-  var left = parseInt(
-    window.getComputedStyle(character).getPropertyValue("left")
-  );
-  character.style.left = left + 2 + "px";
-}
-
-function moveRight() {
   var left = parseInt(
     window.getComputedStyle(character).getPropertyValue("left")
   );
   character.style.left = left - 2 + "px";
 }
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") {
-    interval = setInterval(moveLeft, 1);
-  }
+function moveRight() {
+  var left = parseInt(
+    window.getComputedStyle(character).getPropertyValue("left")
+  );
+  character.style.left = left + 2 + "px";
+}
 
-  if (event.key === "ArrowRight") {
-    interval = setInterval(moveRight, 1);
+document.addEventListener("keydown", (event) => {
+  if (both == 0) {
+    if (event.key === "ArrowLeft") {
+      interval = setInterval(moveLeft, 1);
+    }
+
+    if (event.key === "ArrowRight") {
+      interval = setInterval(moveRight, 1);
+    }
   }
 });
 
 document.addEventListener("keyup", (event) => {
   clearInterval(interval);
+  both = 0;
 });
