afab0fe2927d3ab247515104029d056c11deb872
scriptjumper <shaeendev@gmail.com>
2021-09-02 20:21:15 +0200

    Randomizing hole on platform

--- script.js
+++ script.js
@@ -46,5 +46,8 @@ hole.setAttribute("class", "hole");
 block.setAttribute("id", "block");
 hole.setAttribute("id", "hole");
 
+var random = Math.floor(Math.random() * 360);
+hole.style.left = random + "px";
+
 game.appendChild(block);
 game.appendChild(hole);
