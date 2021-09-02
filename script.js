9330869cda3d9f977970ca5b5bb7e81a09b26d28
scriptjumper <shaeendev@gmail.com>
2021-09-02 20:06:39 +0200

    Creating character movement

---
+++ script.js
@@ -0,0 +1,30 @@
+var character = document.getElementById("character");
+var interval;
+
+function moveLeft() {
+  var left = parseInt(
+    window.getComputedStyle(character).getPropertyValue("left")
+  );
+  character.style.left = left + 2 + "px";
+}
+
+function moveRight() {
+  var left = parseInt(
+    window.getComputedStyle(character).getPropertyValue("left")
+  );
+  character.style.left = left - 2 + "px";
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    interval = setInterval(moveLeft, 1);
+  }
+
+  if (event.key === "ArrowRight") {
+    interval = setInterval(moveRight, 1);
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  clearInterval(interval);
+});
